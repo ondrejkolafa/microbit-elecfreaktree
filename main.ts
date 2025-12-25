202daf3e37ab6e9fcb4df9b3676d3faa8d152034
okolafa <46636140+ondrejkolafa@users.noreply.github.com>
2025-12-25 21:48:22 +0100

    react to sound level

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let intenzita = 0
 let hluk = 0
 let strip = neopixel.create(DigitalPin.P2, 6, NeoPixelMode.RGB)
 let range = strip.range(0, 6)
@@ -5,7 +6,8 @@ strip.setBrightness(10)
 strip.showRainbow(1, 360)
 basic.forever(function () {
     hluk = input.soundLevel()
+    intenzita = hluk * 3
     strip.rotate(1)
     strip.show()
-    basic.pause(250)
+    basic.pause(400 - intenzita)
 })
